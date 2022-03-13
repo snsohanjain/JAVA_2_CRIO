586d7a4d20f2c08301fe263b54d959b4caca6abd
sohanjain <snsohanjain@gmail.com>
2022-03-13 06:50:18 +0530

    Initial commit

---
+++ Single_Inheritance/src/com/sohan/SingleInheritance.java
@@ -0,0 +1,13 @@
+package com.sohan;
+
+public class SingleInheritance {
+    public static void main (String args []){
+
+        method obj = new method();
+        obj.methodA();
+        obj.methodB();
+
+
+    }
+
+}
---
+++ Single_Inheritance/src/com/sohan/method.java
@@ -0,0 +1,15 @@
+package com.sohan;
+
+public class method {
+
+    public void methodA()  {
+        System.out.println("Base class method");
+    }
+
+    public void methodB()
+    {
+        System.out.println("Child class method");
+
+    }
+
+}
